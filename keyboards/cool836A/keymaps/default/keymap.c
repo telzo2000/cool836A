38b6febc8721a37e63b4f0088b3faa98fd7768ba
telzo2000 <67196597+telzo2000@users.noreply.github.com>
2021-01-21 13:10:48 +0900

    Update keymap.c

--- keyboards/cool836A/keymaps/default/keymap.c
+++ keyboards/cool836A/keymaps/default/keymap.c
@@ -4,7 +4,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		KC_ESC, KC_LCTL, KC_LSFT, KC_Y, KC_H, RSHT(KC_SPC), 
+		KC_ESC, KC_LCTL, KC_LSFT, KC_Y, KC_H, RSFT(KC_SPC), 
 		ALT_T(KC_Q), KC_A, KC_Z, KC_U, KC_J, LT(1, KC_N), 
 		KC_W, KC_S, GUI_T(KC_X), KC_I, KC_K, KC_M, 
 		KC_E, KC_D, KC_C, KC_O, KC_L, KC_COMM, 
